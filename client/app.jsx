6e717d499afc39e90ce5a15c56064f978b1a4901
Kevin E Meraz <kevinemeraz@gmail.com>
2016-07-14 19:44:23 -0700

    person suggestions completed

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { EditorState } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin';
+import editorStyles from './editorStyles.css';
+import mentions from './mentions';
+import 'draft-js-mention-plugin/lib/plugin.css';
 
-class App extends React.Component {
+const mentionPlugin = createMentionPlugin();
+const { MentionSuggestions } = mentionPlugin;
+const plugins = [mentionPlugin];
 
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      editorState: EditorState.createEmpty(),
+      suggestions: mentions,
+    };
+    this.onChange = (editorState) => {
+      this.setState({
+        editorState,
+      });
+    };
+
+    this.onSearchChange = ({ value }) => {
+      this.setState({
+        suggestions: defaultSuggestionsFilter(value, mentions),
+      });
+    };
+
+    this.focus = () => {
+      this.refs.editor.focus();
     };
   }
 
   render() {
-    return <h1>Hello, World</h1>;
+    return (
+      <div className={ editorStyles.editor } onClick={ this.focus }>
+        <Editor
+          editorState={ this.state.editorState }
+          onChange={this.onChange}
+          plugins={plugins}
+          ref="editor"
+        />
+        <MentionSuggestions
+          onSearchChange={ this.onSearchChange }
+          suggestions={ this.state.suggestions }
+        />
+      </div>
+    );
   }
-
 }
-
-export default App;
